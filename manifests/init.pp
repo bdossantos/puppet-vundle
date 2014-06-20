62d0422326e7047489d171d056758a3376ebcf82
Benjamin Dos Santos <benjamin.dossantos@gmail.com>
2014-06-21 00:07:42 +0200

    Update Vundle repository address, install target and command.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,19 +1,19 @@
 class vundle($home = '/Users') {
   $vim_dir = "${home}/${::luser}/.vim"
   $bundle_dir = "${vim_dir}/bundle"
-  $vundle_dir = "${bundle_dir}/vundle"
+  $vundle_dir = "${bundle_dir}/Vundle.vim"
 
   file { [$vim_dir, $bundle_dir]:
     ensure   => 'directory',
   } ->
 
   repository { $vundle_dir:
-    source  => 'https://github.com/gmarik/vundle.git',
+    source  => 'https://github.com/gmarik/Vundle.vim',
     notify  => Exec['BundleInstall'],
   } ->
 
   exec { 'BundleInstall':
-    command     => 'vim +BundleInstall +qall',
+    command     => 'vim +PluginInstall +qall',
     subscribe   => Repository[$vundle_dir],
     refreshonly => true,
     path        => $::path,
